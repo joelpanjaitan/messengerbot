b97f41fc575b6f1f3a9ef89d089fb18f4b0c856a
joelpanjaitan <metrohal88@gmail.com>
2021-12-02 17:37:40 +0700

    this is the index.html file as the main interface

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
 const app = express();
 const hostname = "127.0.0.1";
 const port = process.env.PORT || 3000;
+const mongoose = require("mongoose");
 const Controller = require("./controllers/controller");
-app.use(express.json());
-app.get("/", Controller.home);
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// for app routes
+// app.use("/", require("./routes/webhook_verify"));
+app.use(express.static(__dirname));
+// app.get("/", Controller.home);
 
 app.listen(port, () => {
   console.log("App is running on http://" + hostname + ":" + port + "/");
